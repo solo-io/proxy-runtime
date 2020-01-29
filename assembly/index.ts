43134414e90abc4a89cecbde04650ab567b021f3
Yuval Kohavi <yuval.kohavi@gmail.com>
2020-01-29 11:37:42 -0500

    export less

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,25 +1,20 @@
 
 export * from "./malloc";
-export * from "./runtime";
-export * from "./imports";
 export * from "./exports";
 
-import { RootContext, Context, RootContextHelper, ContextHelper, registerRootContext, LogLevelValues, FilterHeadersStatusValues, HeaderMapTypeValues, add_header_map_value_string } from "./runtime"
+///////////////////////////////////////////////
+import { RootContext, Context, RootContextHelper, ContextHelper, registerRootContext, FilterHeadersStatusValues, HeaderMapTypeValues, add_header_map_value_string } from "./runtime"
 
-import { log } from "./runtime"
 class AddHeaderRoot extends RootContext {
   constructor() {
     super();
-    log(LogLevelValues.warn, "AddHeaderRoot created");
   }
-
   createContext(): Context {
     return ContextHelper.wrap(new AddHeader());
   }
 }
 
 class AddHeader extends Context {
-
   onResponseHeaders(a: u32): FilterHeadersStatusValues {
     add_header_map_value_string(HeaderMapTypeValues.ResponseHeaders, "hello", "world!");
     return FilterHeadersStatusValues.Continue;
