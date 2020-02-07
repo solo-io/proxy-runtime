7c2c2e1b403fb26ca6b3ef754bb8031e15c1770c
Yuval Kohavi <yuval.kohavi@gmail.com>
2020-02-07 13:43:55 -0500

    format

--- assembly/runtime.ts
+++ assembly/runtime.ts
@@ -321,23 +321,23 @@ export function send_local_response(response_code: u32, response_code_details: s
 
 export function clear_route_cache(): WasmResultValues { return imports.proxy_clear_route_cache(); }
 
-export function get_shared_data(key: string, value : ArrayBuffer/*, cas*/) : WasmResultValues{
+export function get_shared_data(key: string, value: ArrayBuffer/*, cas*/): WasmResultValues {
   const key_buffer = String.UTF8.encode(key);
   let dummy = globalUsizeRef;
   return imports.proxy_get_shared_data(changetype<usize>(key_buffer), key_buffer.byteLength, changetype<usize>(value), value.byteLength, dummy.ptr());
 }
 
-class SetSharedData{
-  value:ArrayBuffer;
-  result:WasmResultValues;
+class SetSharedData {
+  value: ArrayBuffer;
+  result: WasmResultValues;
 }
-export function set_shared_data(key: string/*, cas*/):SetSharedData {
+export function set_shared_data(key: string/*, cas*/): SetSharedData {
   const key_buffer = String.UTF8.encode(key);
   let dummy = globalUsizeRef;
   let value = globalArrayBufferReference;
   let result = new SetSharedData();
   result.result = imports.proxy_set_shared_data(changetype<usize>(key_buffer), key_buffer.byteLength, value.bufferPtr(), value.sizePtr(), dummy.ptr())
-  if (result.result == WasmResultValues.Ok){
+  if (result.result == WasmResultValues.Ok) {
     result.value = value.toArrayBuffer();
   }
   return result;
@@ -387,58 +387,58 @@ export function add_header_map_value_string(typ: HeaderMapTypeValues, key: strin
 
 
 class HeaderStreamManipulator {
-  typ : HeaderMapTypeValues;
-  constructor(typ : HeaderMapTypeValues){
+  typ: HeaderMapTypeValues;
+  constructor(typ: HeaderMapTypeValues) {
     this.typ = typ;
   }
 
 
-/**
- * Add a header.
- * @param key the header name.
- * @param value the header value.
- */ 
-add(key: string, value: string): void {
- add_header_map_value_string(this.typ, key, value);
-}
+  /**
+   * Add a header.
+   * @param key the header name.
+   * @param value the header value.
+   */
+  add(key: string, value: string): void {
+    add_header_map_value_string(this.typ, key, value);
+  }
 
-/**
- * Replace a header.
- * @param key the header name.
- * @param value the header value.
- */ 
-replace(key: string, value: string): void {
- replace_header_map_value_string(this.typ, key, value);
-}
+  /**
+   * Replace a header.
+   * @param key the header name.
+   * @param value the header value.
+   */
+  replace(key: string, value: string): void {
+    replace_header_map_value_string(this.typ, key, value);
+  }
 
-/**
- * Get a header.
- * @param key the header name.
- * @return the header value.
- */ 
-get(key: string): string {
-  return get_header_map_value_string(this.typ, key);
-}
-/**
- * Remove a header.
- * @param key the header name.
- */ 
-remove(key: string): void {
-  remove_header_map_value_string(this.typ, key);
-}
-/**
- * get all headers.
- */
-get_headers(): Headers {
-  return get_header_map_pairs(this.typ);
-}
+  /**
+   * Get a header.
+   * @param key the header name.
+   * @return the header value.
+   */
+  get(key: string): string {
+    return get_header_map_value_string(this.typ, key);
+  }
+  /**
+   * Remove a header.
+   * @param key the header name.
+   */
+  remove(key: string): void {
+    remove_header_map_value_string(this.typ, key);
+  }
+  /**
+   * get all headers.
+   */
+  get_headers(): Headers {
+    return get_header_map_pairs(this.typ);
+  }
 
-/**
- * set all headers.
- */
-set_headers(headers:Headers): void {
-  return set_header_map_pairs(this.typ, headers);
-}
+  /**
+   * set all headers.
+   */
+  set_headers(headers: Headers): void {
+    return set_header_map_pairs(this.typ, headers);
+  }
 
 }
 
@@ -449,9 +449,9 @@ set_headers(headers:Headers): void {
  * was sent downstream.
  */
 class HeaderMapManipulator {
-  request : HeaderStreamManipulator;
-  response : HeaderStreamManipulator;
-  constructor(request : HeaderStreamManipulator, response : HeaderStreamManipulator){
+  request: HeaderStreamManipulator;
+  response: HeaderStreamManipulator;
+  constructor(request: HeaderStreamManipulator, response: HeaderStreamManipulator) {
     this.request = request;
     this.response = response;
   }
@@ -462,9 +462,9 @@ class HeaderMapManipulator {
  * stream_context variable.
  */
 class StreamContext {
-  headers : HeaderMapManipulator;
-  trailers : HeaderMapManipulator;
-  constructor(headers : HeaderMapManipulator, trailers : HeaderMapManipulator){
+  headers: HeaderMapManipulator;
+  trailers: HeaderMapManipulator;
+  constructor(headers: HeaderMapManipulator, trailers: HeaderMapManipulator) {
     this.headers = headers;
     this.trailers = trailers;
   }
@@ -474,8 +474,8 @@ class StreamContext {
  * Use this variable to manipulate the current stream.
  */
 export var stream_context = new StreamContext(
-  new HeaderMapManipulator(new HeaderStreamManipulator(HeaderMapTypeValues.RequestHeaders),new HeaderStreamManipulator(HeaderMapTypeValues.ResponseHeaders)),
-  new HeaderMapManipulator(new HeaderStreamManipulator(HeaderMapTypeValues.RequestTrailers),new HeaderStreamManipulator(HeaderMapTypeValues.ResponseTrailers)));
+  new HeaderMapManipulator(new HeaderStreamManipulator(HeaderMapTypeValues.RequestHeaders), new HeaderStreamManipulator(HeaderMapTypeValues.ResponseHeaders)),
+  new HeaderMapManipulator(new HeaderStreamManipulator(HeaderMapTypeValues.RequestTrailers), new HeaderStreamManipulator(HeaderMapTypeValues.ResponseTrailers)));
 
 
 function get_header_map_value_string(typ: HeaderMapTypeValues, key: string): string {
@@ -498,7 +498,7 @@ function get_header_map_flat_pairs(typ: HeaderMapTypeValues): ArrayBuffer {
   }
   return new ArrayBuffer(0);
 }
- 
+
 function get_header_map_pairs(typ: HeaderMapTypeValues): Headers {
   let pairs = get_header_map_flat_pairs(typ);
   return deserializeHeaders(pairs);
@@ -569,11 +569,11 @@ export function get_buffer_status(typ: BufferTypeValues): BufferStatusResult {
 }
 
 class Metric {
-  metric_id : u32;
-  
+  metric_id: u32;
+
   constructor(typ: MetricTypeValues, name: string) {
-    let metric_res =  define_metric(typ, name);
-    if (metric_res.result != WasmResultValues.Ok){
+    let metric_res = define_metric(typ, name);
+    if (metric_res.result != WasmResultValues.Ok) {
       throw new Error("can't define metric")
     }
     this.metric_id = metric_res.metric_id;
@@ -581,13 +581,13 @@ class Metric {
 }
 
 export class Gauge extends Metric {
-  metric_id : u32;
-  
+  metric_id: u32;
+
   constructor(name: string) {
     super(MetricTypeValues.Gauge, name);
   }
-  
- increment(offset: i64): WasmResultValues {
+
+  increment(offset: i64): WasmResultValues {
     return imports.proxy_increment_metric(this.metric_id, offset);
   }
   record(metric_id: u32, value: u64): WasmResultValues {
@@ -596,13 +596,13 @@ export class Gauge extends Metric {
 }
 
 export class Histogram extends Metric {
-  metric_id : u32;
-  
+  metric_id: u32;
+
   constructor(name: string) {
     super(MetricTypeValues.Histogram, name);
   }
-  
- increment(offset: i64): WasmResultValues {
+
+  increment(offset: i64): WasmResultValues {
     return imports.proxy_increment_metric(this.metric_id, offset);
   }
   record(metric_id: u32, value: u64): WasmResultValues {
@@ -611,13 +611,13 @@ export class Histogram extends Metric {
 }
 
 export class Counter extends Metric {
-  metric_id : u32;
-  
+  metric_id: u32;
+
   constructor(name: string) {
     super(MetricTypeValues.Counter, name);
   }
-  
- increment(offset: u32): WasmResultValues {
+
+  increment(offset: u32): WasmResultValues {
     return imports.proxy_increment_metric(this.metric_id, offset);
   }
 }
@@ -669,6 +669,9 @@ export function set_effective_context(effective_context_id: u32): WasmResultValu
 export function done(): WasmResultValues { return imports.proxy_done(); }
 /////// runtime support
 
+/**
+ * BaseContexts contains things that are common to RootContext and Context.
+ */
 export abstract class BaseContext {
   context_id: u32;
   onDone_: (thiz: BaseContext) => bool;
@@ -679,7 +682,15 @@ export abstract class BaseContext {
     this.onDelete_ = (thiz: BaseContext) => { thiz.onDelete(); };
   }
 
-  setEffectiveContext(): WasmResultValues{
+  /**
+   * Sets the effective context id to this context. this is useful for example if you receive an
+   * http call in a RootContext, and want to modify headers based on the response in a regular 
+   * Context. You then will call `context.setEffectiveContext()` so that the header manipulation will
+   * occur in the requerst context and not in the root context.
+   * the r
+   * @param effective_context_id 
+   */
+  setEffectiveContext(): WasmResultValues {
     return imports.proxy_set_effective_context(this.context_id);
   }
 
@@ -688,7 +699,7 @@ export abstract class BaseContext {
   onDelete(): void { } // Called when the VM is being torn down.
 }
 
-// we have to use a wrapper as asm script doesn't support closures just yet.
+// We have to use a wrapper as asm script doesn't support closures just yet.
 class HttpCallback {
   ctx: Object;
   cb: (c: Object) => void;
@@ -707,6 +718,10 @@ class GrpcCallback {
   }
 }
 
+/**
+ * A root context represents a class of instance specific contexts. This is usually used to hold
+ * configuration that is used in the individual contexts.
+ */
 export class RootContext extends BaseContext {
   // hack to workaround lack of OOP
   validateConfiguration_: (thiz: RootContext, configuration_size: size_t) => bool;
@@ -715,7 +730,7 @@ export class RootContext extends BaseContext {
   onTick_: (thiz: RootContext) => void;
   done_: (thiz: RootContext) => void;
   createContext_: (thiz: RootContext) => Context;
-  onQueueReady_: (thiz: RootContext, token:u32) => void;
+  onQueueReady_: (thiz: RootContext, token: u32) => void;
 
   private http_calls_: Map<u32, HttpCallback>;
   private grpc_calls_: Map<u32, GrpcCallback>;
@@ -728,7 +743,7 @@ export class RootContext extends BaseContext {
     this.onConfigure_ = (thiz: RootContext, configuration_size: size_t) => { return thiz.onConfigure(); };
     this.onStart_ = (thiz: RootContext, vm_configuration_size: size_t) => { return thiz.onStart(vm_configuration_size); };
     this.onTick_ = (thiz: RootContext) => { thiz.onTick(); };
-    this.onQueueReady_ = (thiz: RootContext, token:u32) => { thiz.onQueueReady(token); };
+    this.onQueueReady_ = (thiz: RootContext, token: u32) => { thiz.onQueueReady(token); };
     this.createContext_ = (thiz: RootContext) => { return thiz.createContext(); };
     this.onDone_ = (thiz: BaseContext) => { return (thiz as RootContext).onDone(); };
 
@@ -745,7 +760,7 @@ export class RootContext extends BaseContext {
   }
 
   /**
-   * cancels all pending http requests.
+   * Cancels all pending http requests. Called automatically on onDone.
    */
   cancelPendingRequests(): void {
     let keys = this.http_calls_.keys();
@@ -775,6 +790,15 @@ export class RootContext extends BaseContext {
     throw new Error("not implemented");
   }
 
+  /**
+   * Make an http call.
+   * @param cluster The cluster name to make http request to.
+   * @param headers The headers of the request.
+   * @param body The body of the request.
+   * @param trailers The trailers of the request.
+   * @param timeout_milliseconds Timeout for the request, in milliseconds.
+   * @param cb Callback to be invoked when the request is complete.
+   */
   httpCall(cluster: string, headers: Headers, body: ArrayBuffer, trailers: Headers,
     timeout_milliseconds: u32, cb: HttpCallback): WasmResultValues {
 
@@ -822,7 +846,10 @@ export class RootContext extends BaseContext {
 
 }
 
-export class Context extends BaseContext{
+/**
+ * Context class the base for class for entities that are per-request or per-connection.
+ */
+export class Context extends BaseContext {
 
   onNewConnection_: (thiz: Context) => FilterStatusValues;
   onDownstreamData_: (thiz: Context, size: size_t, end: bool) => FilterStatusValues;
@@ -894,13 +921,13 @@ export function ensureRootContext(root_context_id: u32): RootContext {
     let root_context = root_context_func();
     root_context.context_id = root_context_id;
     context_map.set(root_context_id, root_context);
-    
+
     log(LogLevelValues.warn, "returning context for " + root_id);
     return root_context;
   }
-  
+
   log(LogLevelValues.warn, "did not find root id " + root_id)
-  
+
   let root_context = new RootContext();
   root_context.context_id = root_context_id;
   context_map.set(root_context_id, root_context);
@@ -923,6 +950,7 @@ export function getRootContext(context_id: u32): RootContext {
   return context_map.get(context_id) as RootContext;
 }
 
+// create a context if doesnt exist.
 export function ensureContext(context_id: u32, root_context_id: u32): void {
   if (context_map.has(context_id)) {
     return;
@@ -934,6 +962,11 @@ export function ensureContext(context_id: u32, root_context_id: u32): void {
 }
 
 
+/**
+ * Because asm script doest support virtual functions, we need to wrap root contexts with this wrapper.
+ * this wrapper manually implements virtual functions. Use it via the wrap class method.
+ * once asm script is updated to support virtual functions this will become a no-op.
+ */
 export class RootContextHelper<T extends RootContext> extends RootContext {
   static wrap<T extends RootContext>(that: T): RootContext {
     return new RootContextHelper<T>(that);
@@ -947,7 +980,7 @@ export class RootContextHelper<T extends RootContext> extends RootContext {
     this.onConfigure_ = (thiz: RootContext, configuration_size: size_t) => { return (thiz as RootContextHelper<T>).that.onConfigure(); };
     this.onStart_ = (thiz: RootContext, vm_configuration_size: size_t) => { return (thiz as RootContextHelper<T>).that.onStart(vm_configuration_size); };
     this.onTick_ = (thiz: RootContext) => { (thiz as RootContextHelper<T>).that.onTick(); };
-    this.onQueueReady_ = (thiz: RootContext, token:u32) => { (thiz as RootContextHelper<T>).that.onQueueReady(token); };
+    this.onQueueReady_ = (thiz: RootContext, token: u32) => { (thiz as RootContextHelper<T>).that.onQueueReady(token); };
     this.onDone_ = (thiz: BaseContext) => { return (thiz as RootContextHelper<T>).that.onDone(); };
     this.done_ = (thiz: RootContext) => { (thiz as RootContextHelper<T>).that.done(); };
     this.createContext_ = (thiz: RootContext) => { return (thiz as RootContextHelper<T>).that.createContext(); };
@@ -955,7 +988,11 @@ export class RootContextHelper<T extends RootContext> extends RootContext {
   }
 }
 
-
+/**
+ * Because asm script doest support virtual functions, we need to wrap contexts with this wrapper.
+ * this wrapper manually implements virtual functions. Use it via the wrap class method.
+ * once asm script is updated to support virtual functions this will become a no-op.
+ */
 export class ContextHelper<T extends Context> extends Context {
   static wrap<T extends Context>(that: T): Context {
     return new ContextHelper<T>(that);
@@ -984,6 +1021,11 @@ export class ContextHelper<T extends Context> extends Context {
   }
 }
 
+/**
+ * Register a root cotnext and make it available to the runtime.
+ * @param c A function that creates a new root context.
+ * @param name The name of the root context. This should match the name configured in the proxy.
+ */
 export function registerRootContext(c: () => RootContext, name: string): void {
   root_factory.set(name, c);
 }
