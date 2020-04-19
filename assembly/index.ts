be1a207cecfa4c0772ad20f05706eecd7a21c85a
Yuval Kohavi <yuval.kohavi@gmail.com>
2020-04-19 17:16:17 -0400

    auth example (#14)

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,12 +1,12 @@
 export {
     BaseContext, RootContext, Context, RootContextHelper, ContextHelper, registerRootContext,
-    LogLevelValues, WasmResultValues,
+    LogLevelValues, WasmResultValues, GrpcStatusValues,
     FilterStatusValues,
     FilterHeadersStatusValues,
     FilterMetadataStatusValues,
     FilterTrailersStatusValues,
     FilterDataStatusValues, stream_context,
-    HeaderPair, Headers, 
-    Gauge, Histogram, Counter, log, 
-    HttpCallback, proxy_set_effective_context
+    HeaderPair, Headers,
+    Gauge, Histogram, Counter, log,
+    HttpCallback, send_local_response, continue_request, continue_response, proxy_set_effective_context
 } from "./runtime";
