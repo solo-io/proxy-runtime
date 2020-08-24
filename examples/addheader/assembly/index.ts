49de844f88b02f889ef32a8b47ce161ecea65926
Yuval Kohavi <yuval.kohavi@gmail.com>
2020-08-24 17:52:58 -0400

    fix example

--- examples/addheader/assembly/index.ts
+++ examples/addheader/assembly/index.ts
@@ -1,17 +1,14 @@
 
 export * from "@solo-io/proxy-runtime/proxy"; // this exports the required functions for the proxy to interact with us.
-import { RootContext, Context, RootContextHelper, ContextHelper, registerRootContext, FilterHeadersStatusValues, stream_context } from "@solo-io/proxy-runtime";
+import { RootContext, Context, registerRootContext, FilterHeadersStatusValues, stream_context } from "@solo-io/proxy-runtime";
 
 class AddHeaderRoot extends RootContext {
-  configuration : string;
-
   createContext(context_id: u32): Context {
     return new AddHeader(context_id, this);
   }
 }
 
 class AddHeader extends Context {
-  root_context : AddHeaderRoot;
   constructor(context_id: u32, root_context:AddHeaderRoot){
     super(context_id, root_context);
     this.root_context = root_context;
